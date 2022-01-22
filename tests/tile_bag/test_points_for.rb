3e8f9a8741865901a39f4f8bc666f19c64a3866e
Mike Brown <MBrown3@paylocity.com>
2022-01-22 04:55:23 -0700

    finishing first test

--- tests/tile_bag/test_points_for.rb
+++ tests/tile_bag/test_points_for.rb
@@ -6,7 +6,19 @@ class TileBag::TestPointsFor  < Minitest::Test
     @tile_bag = TileBag.new
   end
 
+  def do_point_test letter, score
+    assert_equal score, TileBag::points_for(letter)
+  end
+
   def test_confirm_point_values
-    assert_equal 1, TileBag::points_for(:E)
+    [:E, :A, :I, :O, :N, :R, :T, :L, :S, :U].each { |l| do_point_test l, 1 }
+    [:D, :G].each { |l| do_point_test l, 2 }
+    [:B, :C, :M, :P].each { |l| do_point_test l, 3 }
+    [:F, :H, :V, :W, :Y].each { |l| do_point_test l, 4 }
+    assert_equal 5, TileBag::points_for(:K)
+    [:J, :X].each { |l| do_point_test l, 8 }
+    [:Q, :Z].each { |l| do_point_test l, 10 }
+
+
   end
 end
